c1310765a68b615f24c1f23463abf78684b1b8ee
sabbir1257 <masabbir1257@gmail.com>
2024-12-11 01:02:30 +0600

    server Setup complete

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express")
-const cors = require("cors")
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 4000;
@@ -11,6 +12,28 @@ app.use(express.json())
 
 
 // mongodb
+const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.o6aj5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(url, {
+     serverApi:{
+          version: ServerApiVersion.v1,
+          strict: true,
+          deprecationErrors: true
+     }
+})
+
+const dbConnet = async() => {
+     try{
+          client.connect()
+          console.log("Database connected successfully");
+     }
+     catch(error){
+          console.log(error.name, error.message)
+     }
+};
+
+dbConnet();
+
 
 
 
